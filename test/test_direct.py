afa7394480e350e7f23212dce72a181e61d55810
Andreas Mayer <andisspam@gmail.com>
2016-05-18 20:18:53 +0200

    making test discoverable

--- test/test_direct.py
+++ test/test_direct.py
@@ -1,11 +1,15 @@
 from scipydirect import minimize
 import numpy as np
+import numpy.testing as npt
 
 def func(x):
     x -=  np.array([-1, 2, -4, 3])
     return np.dot(x, x)
 
-if __name__ == '__main__':
+def test_minimize():
     bounds = [(-10, 10) for i in range(4)]
     res = minimize(func, bounds)
-    print res
+    npt.assert_allclose(res.x, np.array([-1, 2, -4, 3]), atol=0.1)
+
+if __name__ == '__main__':
+    test_minimize()
